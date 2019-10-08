a1a330444a17bd0ba8cee35d3a007757acf7c475
Jazmin M <jazminmerckel@gmail.com>
2019-10-08 03:10:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,13 +12,10 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  
   birthday_kids.each do |kids_name, age|
     if age <= 12
-      puts 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
+end
 
-
-winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
